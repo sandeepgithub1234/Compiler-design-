c52d2ab940a1958c75b2331a4aece81b0064377b
sandeep kumar <53126815+sandeepgithub1234@users.noreply.github.com>
2020-01-10 14:31:55 +0530

    Add files via upload

---
+++ sandeep.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include"calculation.h"
+
+int main()
+{
+  int m=mul(4,5);
+  printf(" sum of two number:%d\n",sum(2,3));
+  printf("multiplication of two number is:%d\n",m);
+ return 0;
+}
